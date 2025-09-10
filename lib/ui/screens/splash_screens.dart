01a53a9c4d8d7bc4b6cdde1d73108adabbe56abc
alamn <alamnahid1205@gmail.com>
2025-09-11 01:30:56 +0600

    Add splash screen ui design

--- lib/ui/screens/splash_screens.dart
+++ lib/ui/screens/splash_screens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:task_manager/ui/screens/utils/asset_paths.dart';
+import 'package:task_manager/ui/widgets/screen_background.dart';
+import '../utils/asset_paths.dart';
 import 'login_screen.dart';
 
 class SplashScreens extends StatefulWidget {
@@ -11,7 +12,6 @@ class SplashScreens extends StatefulWidget {
 }
 
 class _SplashScreensState extends State<SplashScreens> {
-
   @override
   void initState() {
     super.initState();
@@ -19,23 +19,15 @@ class _SplashScreensState extends State<SplashScreens> {
   }
 
   Future<void> _moveToNextScreen() async {
-    await Future.delayed(Duration(seconds: 3));
+    await Future.delayed(Duration(seconds: 1));
     Navigator.push(context, MaterialPageRoute(builder: (_) => LoginScreen()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SvgPicture.asset(
-            AssetPaths.backgroundSvg,
-            height: double.infinity,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          Center(child: SvgPicture.asset(AssetPaths.logoSvg, height: 40)),
-        ],
+      body: ScreenBackground(
+        child: Center(child: SvgPicture.asset(AssetPaths.logoSvg, height: 40)),
       ),
     );
   }
